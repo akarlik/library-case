0b4575ad8e1120f9f2b687ee9104d887baf13dba
ahmetkarlik <karlik.ahmet@gmail.com>
2024-12-16 00:02:54 +0300

    ignored sequelize time fields

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,21 +1,26 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
-  host:process.env.DB_HOST,
-  dialect: 'postgres',
-  logging: false,  
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME!,
+  process.env.DB_USER!,
+  process.env.DB_PASS!,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+    define: {
+      timestamps: false,
+    },
+  }
+);
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Database connected!');
+    console.log("Database connected!");
   })
   .catch((error) => {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   });
 
 export default sequelize;
-
-
-
