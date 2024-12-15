fcd2beb82adbf641e288ba5610a66c35e560c4a9
ahmetkarlik <karlik.ahmet@gmail.com>
2024-12-16 00:07:44 +0300

    added getAllUsers

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,11 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import { json } from 'body-parser';
-import sequelize from "./config/database";
+import userRoutes from './routes/user.route';
 
 const app: Application = express();
 
 app.use(json());
 
-app.get('/', (req, res) => {
-  res.send('API is working');
-});
+app.use('/', userRoutes);
 
-app.get("/test-db", async (req: Request, res: Response) => {
-  try {
-    const result = await sequelize.authenticate();
-    res.json({ success: true, });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ success: false, error: "Database query failed" });
-  }
-});
 export default app;
